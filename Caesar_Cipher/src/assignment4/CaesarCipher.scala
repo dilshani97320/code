14a1015b71a827348d74b3dd458155a4e8119f3d
dilshani97320 <65662091+dilshani97320@users.noreply.github.com>
2020-07-16 20:34:04 +0530

    Add files via upload

---
+++ Caesar_Cipher/src/assignment4/CaesarCipher.scala
@@ -0,0 +1,40 @@
+package assignment4
+
+object CaesarCipher {
+  
+ def main(args:Array[String])
+ {
+   
+     val alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     
+    //Encryption function
+   
+    val Encryption=(c:Char,key:Int,a:String)=>
+                 a((a.indexOf(c.toUpper)+key)%a.size)
+     
+    //Decryption function
+     
+     val Decryption=(c:Char,key:Int,a:String)=>
+                 a((a.indexOf(c.toUpper)-key)%a.size)
+     
+     
+     val cipher=(algo:(Char,Int,String)=>
+                Char,s:String,key:Int,a:String)=>
+                s.map(algo(_,key,a))
+                
+                
+           
+                
+               val message="HappyBirthday";
+               val key=1;
+              
+                
+               val encrypted_text=cipher(Encryption,message,key,alphabet);
+               val plain_text=cipher(Decryption,encrypted_text,key,alphabet);
+               
+                println("Encrypted text: "+encrypted_text)
+
+                println("Real(Decrypt) text: "+plain_text)
+ }
+ 
+}
